6a371c08c1ab5faee6cf4fcdface78f1b0b7aac9
Nina Potrebich <potrebich@gmail.com>
2019-10-23 22:30:45 -0700

    add list

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
+var i = 0;
 $(function () {
   $("form.addressForm").submit(function(event) {
     event.preventDefault();
     var fullName = $("#firstName").val() + " " + $("#lastName").val();
-    $("#contactList").append('<li>'+fullName+'</li>');
-    $("#phone").text();
-    $("#email").text();
-    $("#address").text();
-    // $(".addressForm").hide();
+    i+=1;
+    var extraInfo = '<div id="person' + i + '"><p>Phone: ' + $("#phone").val() +'</p><p>Email: ' + $("#email").val() + '</p><p>Address: ' + $("#address").val() + '</p></div>';
+
+    $("#contactList").append('<li id="contact' + i + '">'+fullName+'</li>');
+    $("#contactList").append(extraInfo);
+    $("#contactList").children().last().hide();
     $(".contactList").show();
+    var j = i;
+    $("#contact" + i).click(function() {
+      $("#person" + j).toggle();
+    })
   });
 });
